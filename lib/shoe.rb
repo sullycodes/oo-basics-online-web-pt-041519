c214c06576e7c17a2b0e8db21651fb4c29b3c6e2
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-08 18:33:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   
   def cobble
-    puts "The shoe has been prepared."
+    puts "The Shoe has been prepared."
     @condition = "new"
   end 
   
