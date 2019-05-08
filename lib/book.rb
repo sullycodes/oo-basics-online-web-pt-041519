2e9f175e59f443248588e4a9ef2b84e4353948b6
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-08 18:34:54 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
-
-
+class Book
+  def initialize(title) #automatically creates when using .new - this becomes :title setter
+    @title = title
+  end
+  
+  attr_reader :title #creates a getter
+  attr_accessor :page_count, :genre #creates a setter & getter for each item
+  
+  def author=(author) #setter
+    @author = author
+  end
+  
+  def author #getter
+    @author
+  end
+  
+  def turn_page
+      puts "Flipping the page...wow, you read fast!"
+  end
+end
