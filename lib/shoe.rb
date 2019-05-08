2f0d463b06cf45041a3b9076f0ba95d42fd89384
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-08 18:15:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+class Book
+  def initialize(title) #automatically creates when using .new - this becomes :title setter
+    @title = title
+  end
+  
+  attr_reader :title #creates a getter
+  attr_accessor :page_count, :genre #creates a setter & getter for each item
+  
+  def author=(author) #setter
+    @author = author
+  end
+  
+  def author #getter
+    @author
+  end
+  
+  def turn_page
+      puts "Flipping the page...wow, you read fast!"
+  end
+end
